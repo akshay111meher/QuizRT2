51dcf236647bc6bd4604c6bd4374b90c9917c925
akshay111meher <akshay_111meher@hotmail.com>
2016-01-11 14:10:57 +0530

    Express routing for index page done

---
+++ app.js
@@ -0,0 +1,15 @@
+var express = require('express');
+var app = express();
+var http = require('http');
+var server = http.createServer(app);
+app.set('view engine', 'ejs');
+app.set('views', './views');
+app.use(express.static('./public'));
+
+app.get('/',function(req,res){
+  res.render('index');
+});
+
+server.listen(3000, function() {
+  console.log('App started for EJS testing!!');
+});
