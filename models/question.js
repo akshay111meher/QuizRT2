23834f114ba23020e8b8677ea13b934172656f69
akshay111meher <akshay_111meher@hotmail.com>
2016-01-11 12:35:59 +0530

    quiz and question models created

---
+++ models/question.js
@@ -0,0 +1,15 @@
+var mongoose = require('mongoose');
+
+
+var questionSchema = mongoose.Schema({
+  questionId: {type:String, unique:true},
+  question : {type:String},
+  option:[],
+  correctIndex: Number,
+  image: {type:String},
+  topicId:[]
+});
+
+Question = mongoose.model('Question', quizSchema,'questions_demo_collection');
+
+module.exports = Quiz;
