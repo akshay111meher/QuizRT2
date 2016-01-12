ad698d316888ab51ff5234d008dd8bfd95735d2b
kshitijjain <jn.kshitij@gmail.com>
2016-01-12 13:10:06 +0530

    question bank pushed

--- models/question.js
+++ models/question.js
@@ -3,12 +3,12 @@ var mongoose = require('mongoose');
 
 var questionSchema = mongoose.Schema({
   questionId: {type:String, unique:true},
-  question : {type:String},
-  options:[],
-  correctIndex: Number,
-  image: {type:String},
-  topicId:[]
-});
+  image: String,
+  question : String,
+   correctIndex: Number,
+  options: Array,
+  topicId:Array
+}, {strict: false});
 
 Question = mongoose.model('Question', questionSchema,'question_bank_collection');
 
