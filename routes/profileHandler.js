c0607c88f2670d72ac20441d9cf4530fdfb06007
unknown <raghavgoel26@gmail.com>
2016-01-12 17:08:36 +0530

    some changes in app.js

--- routes/profileHandler.js
+++ routes/profileHandler.js
@@ -1,3 +1,7 @@
 var express = require('express');
 var router = express.Router();
 var mongoose = require( 'mongoose' );
+
+
+
+module.exports= router;
--- routes/topicsHandler.js
+++ routes/topicsHandler.js
@@ -1,3 +1,21 @@
 var express = require('express');
 var router = express.Router();
 var mongoose = require( 'mongoose' );
+var bodyParser = require('body-parser');
+
+
+console.log(router);
+var Category = require()
+
+// router.route('/categories')
+ 	//gets all categories
+// 	.get(function(req, res){
+// 		Post.find(function(err, posts){
+// 			if(err){
+// 				return res.send(500, err);
+// 			}
+// 			return res.send(posts);
+// 		});
+// 	});
+
+module.exports= router;
