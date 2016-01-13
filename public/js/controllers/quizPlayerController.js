3703ad4dc19370e24d8a702483a4068e948fe488
akshay111meher <akshay_111meher@hotmail.com>
2016-01-13 15:48:24 +0530

    latest version

--- public/js/controllers/quizPlayerController.js
+++ public/js/controllers/quizPlayerController.js
@@ -1,13 +1,79 @@
+var questionCounter = 0;
+var temp;
 angular.module('quizRT')
-	.controller('quizPlayerController', function($scope, $interval){
-  		$scope.time=10;
+	.controller('quizPlayerController', function($scope, $interval,$http){
 
-  		var timeInterval= $interval(function(){
-  			$scope.time--;
+		$http.post('/quizPlayer/quizData')
+				.success(function(data, status, headers, config) {
 
-  			if($scope.time==0){
-  				$scope.time=10;
-  			}
-  		},1000)
 
+								$scope.time=3;
+					  		var timeInterval= $interval(function(){
+					  			$scope.time--;
+
+									if($scope.time == 0){
+										$scope.isDisabled = false;
+										$scope.myImage = "/images/userProfileImages/akshay.jpg"
+										$scope.topperImage = "/images/userProfileImages/akshayk.jpg"
+										if(questionCounter == data.questions.length){
+											$interval.cancel(timeInterval);
+										}
+										else{
+											temp = loadNextQuestion(data,questionCounter);
+											$scope.changeColor = function(id,element){
+
+													if(id == temp.correctIndex){
+						                $(element.target).addClass('btn-success');
+						              }
+						              else{
+						                $(element.target).addClass('btn-danger');
+						                angular.element('#option'+temp.correctIndex).addClass('btn-success');
+						                $scope.$emit('emitCurrentScore','wrongAnswer');
+						              }
+						              $scope.isDisabled = true;
+						            };
+
+											$scope.question = temp.question;
+											$scope.options = temp.options;
+
+											if(temp.image != "null")
+											$scope.questionImage = temp.image;
+
+											else{
+												$scope.questionImage = null;
+											}
+											$scope.time = 10;
+										}
+									}
+
+					  		},1000);
+
+					console.log(data);
+
+				})
+				.error(function(data, status, headers, config) {
+					console.log(error);
+				});
  });
+
+function loadNextQuestion(data,questionNumber){
+	var optionCoutner = 0;
+	var obj;
+	var options=[];
+	while(data.questions[questionNumber].options[optionCoutner]){
+		opt = {
+				name:data.questions[questionNumber].options[optionCoutner],
+				id:"option"+(optionCoutner+1)
+		};
+		options.push(opt);
+		optionCoutner++;
+	}
+	obj = {
+		"options":options,
+		"question":data.questions[questionNumber].question,
+		"image":data.questions[questionNumber].image,
+		"correctIndex":data.questions[questionNumber].correctIndex
+	}
+	questionCounter++;
+	return obj;
+}
