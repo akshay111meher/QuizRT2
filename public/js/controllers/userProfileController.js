4ee63952a2c619aad440a73351d1f652eab6fdf6
lakshaybansal <blakshay93@gmail.com>
2016-01-13 10:46:14 +0530

    Successfully displaying the profile data through angular and changed the controller as well as userprofile.html

--- public/js/controllers/userProfileController.js
+++ public/js/controllers/userProfileController.js
@@ -1,7 +1,7 @@
 angular.module('quizRT')
  .controller('userProfileController',['$http','$scope',function($http,$scope){
       console.log("In Profile controller Testing");
-      $http({method : 'POST',url:'/userProfile/profileData'})
+      $http({method : 'GET',url:'/userProfile/profileData'})
        .success(function(data){
          console.log(data);
          $scope.data = data;
