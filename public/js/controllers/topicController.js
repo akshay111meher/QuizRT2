7e4578222ecb11795a2598e110ead8aa4bd5014c
unknown <raghavgoel26@gmail.com>
2016-01-14 19:02:04 +0530

    topic page updated

--- public/js/controllers/topicController.js
+++ public/js/controllers/topicController.js
@@ -1,18 +1,18 @@
 angular.module('quizRT')
-  .controller('topicController', function($scope,$rootScope){
+  .controller('topicController', function($scope,$rootScope,$routeParams){
 
-    $scope.topicID=$routeParams.topicID;
-    $scope.topic="";
-    $rootScope.stylesheetName="topic";
-
-    var path = '/topicsHandler/topic/'+$scope.topicID;
-    $http.get(path)
-          .success(function(data, status, headers, config) {
-            $scope.topic = data;
-          })
-          .error(function(data, status, headers, config) {
-            console.log(error);
-          });
+     $scope.topicID=$routeParams.topicID;
+     $scope.topic="";
+     $rootScope.stylesheetName="topic";
+    //
+    // var path = '/topicsHandler/topic/'+$scope.topicID;
+    // $http.get(path)
+    //       .success(function(data, status, headers, config) {
+    //         $scope.topic = data;
+    //       })
+    //       .error(function(data, status, headers, config) {
+    //         console.log(error);
+    //       });
 
 
   });
