81e5c615ed92b860971d49a3be48bed98c8d95fd
lakshaybansal <blakshay93@gmail.com>
2016-01-14 10:33:57 +0530

    Successfully displaying profile page and linking to categories page

--- public/js/controllers/userProfileController.js
+++ public/js/controllers/userProfileController.js
@@ -2,16 +2,15 @@ angular.module('quizRT')
 
  .controller('userProfileController',['$http','$scope',function($http,$scope)
  {
+   $scope.a=11;
+   $scope.seeall=function(length){
+     console.log(length);
+     $scope.a=length;
+   }
       console.log("In Profile controller Testing");
       $http({method : 'GET',url:'/userProfile/profileData'})
        .success(function(data){
          console.log(data);
          $scope.data = data;
-         $scope.a=11;
-         $scope.seeall=function(length)
-         {
-           console.log(length);
-           $scope.a=length;
-         }
        });
  }]);
