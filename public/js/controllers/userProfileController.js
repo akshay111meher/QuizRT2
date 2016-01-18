5706d2ded341f8cafd69500c7e0d3f3fb5dac5e7
unknown <raghavgoel26@gmail.com>
2016-01-18 18:01:01 +0530

    changes in userprofile working

--- public/js/controllers/userProfileController.js
+++ public/js/controllers/userProfileController.js
@@ -1,66 +1,5 @@
 angular.module('quizRT')
-<<<<<<< HEAD
-    
-  $rootScope.authenticated=true;
-  console.log("sbjndjs"+ $rootScope.authenticated);
- .controller('userProfileController',function($http,$scope,$rootScope,$location)
- {
-    
-   $http.get('/auth/checkAuth')
-           .success(function(data, status, headers, config) {
-                          console.log("############"+ data);
-                          $rootScope.authenticated = data;
-                        })
-           .error(function(data, status, headers, config) {
-                          console.log(error);
-                        });
-           console.log($rootScope.authenticated);
-   if($rootScope.authenticated==false) $location.path('/login');
-
-
-   $rootScope.stylesheetName="userProfile";
-
-   $scope.a=11;
-   $scope.see = true;
-
-   $scope.btnImg = "images/userProfileImages/seeall.jpg";
-   $scope.seeHide=function(length){
-     if($scope.see){
-       $scope.see = false;
-       $scope.btnImg = "images/userProfileImages/hide.jpg";
-       $scope.a=length;
-     }
-     else{
-       $scope.see = true;
-       $scope.btnImg = "images/userProfileImages/seeall.jpg";
-       $scope.a=11;
-     }
-
-   }
-
-
-
-      console.log("In Profile controller Testing");
-      $http({method : 'GET',url:'/userProfile/profileData'})
-       .success(function(data){
-         console.log(data);
-         $scope.data = data;
-         $scope.topicsFollowed = [];
-         var k = 0;
-         for(var i = 0;i < $scope.data.topicsPlayed.length;i++){
-            if($scope.data.topicsPlayed[i].isFollowed){
-              $scope.topicsFollowed[k] =$scope.data.topicsPlayed[i];
-              k++;
-            }
-         }
-         $scope.showFollowedTopic=function(topicID){
-           console.log(topicID);
-           var path = '/topic/'+topicID;
-           console.log(path);
-           $location.path(path);
-         };
-=======
- .controller('userProfileController',['$http','$scope','$rootScope','$location',function($http,$scope,$rootScope,$location){
+ .controller('userProfileController',function($http,$scope,$rootScope,$location){
      $rootScope.stylesheetName="userProfile";
      $scope.a=11;
      $scope.see = true;
@@ -71,7 +10,6 @@ angular.module('quizRT')
          $scope.see = false;
          $scope.btnImg = "images/userProfileImages/hide.jpg";
          $scope.a=length;
->>>>>>> f11fd175f1c9136986c50904d8b9f95a953adcb9
        }
        else{
          $scope.see = true;
@@ -80,9 +18,6 @@ angular.module('quizRT')
        }
      }
 
-<<<<<<< HEAD
- });
-=======
     $http({method : 'GET',url:'/userProfile/profileData'})
       .success(function(data){
         $scope.data = data;
@@ -99,5 +34,4 @@ angular.module('quizRT')
           $location.path(path);
         };
       });
- }]);
->>>>>>> f11fd175f1c9136986c50904d8b9f95a953adcb9
+ });
--- routes/authenticationHandler.js
+++ routes/authenticationHandler.js
@@ -3,39 +3,17 @@ var router = express.Router();
 
 module.exports = function(passport){
 
-
-	router.route('/checkAuth')
-
-  		.get(function(req, res){
-
-		return res.send(req.session.isAuthenticated);
- 	});
-
-
-
-	
 	//sends successful login state back to angular
 	router.get('/success', function(req, res){
 		req.session.user = req.user.username;
-		req.session.isAuthenticated= true;
 		console.log("this is session object");
-<<<<<<< HEAD
-		console.log(req.user);
-		//  console.log(req.user.username + "$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$");
-		// console.log(req.session);
-		//  console.log(req.user.username + "$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$");
-		// console.log(req.user.username + "$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$");
-		res.send({state: 'success', isAuthenticated: req.session.isAuthenticated ,user: req.user ? req.user : null});
-=======
 
 		res.send({state: 'success', user: req.user ? req.user : null});
->>>>>>> f11fd175f1c9136986c50904d8b9f95a953adcb9
 	});
 
 	//sends failure login state back to angular
 	router.get('/failure', function(req, res){
-		req.session.isAuthenticated=false;
-		res.send({state: 'failure', user: null, message: "Invalid username or password", isAuthenticated: req.session.isAuthenticated});
+		res.send({state: 'failure', user: null, message: "Invalid username or password"});
 	});
 
 	//log in
@@ -52,10 +30,7 @@ module.exports = function(passport){
 
 	//log out
 	router.get('/logout', function(req, res) {
-		req.session.isAuthenticated=false;
-		console.log('logout function called');
-		req.session.destroy();
-		// delete req.session.user;
+		req.logout();
 	});
 
 	return router;
